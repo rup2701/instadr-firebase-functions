2d743b2c3f1de423966066aea52f2e7b0eeb7314
Rupesh Pandey <rupesh@creativegray.com>
2021-05-20 16:10:00 -0700

    Modified logic to store appts keyed on appts_date instead of creating a push_key. Updates happen seamlessly using the appt_date. Code clean up and refactorings.

--- functions/appointments.js
+++ functions/appointments.js
@@ -5,7 +5,7 @@ const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 // const request = require('request-promise');
 
-exports.appointment = functions.database.ref('/appointments/{doctorId}/{date}/{time}').onWrite(async (change, ctx) => {
+exports.appointment = functions.database.ref('/appointments/{doctorId}/{appt_date}/{appt_time}').onWrite(async (change, ctx) => {
   console.log('New appt added for doctor with ID', ctx.params.doctorId);
   
   // if before value does not exist - the appt is still on hold and 
@@ -17,16 +17,14 @@ exports.appointment = functions.database.ref('/appointments/{doctorId}/{date}/{t
   // prepare updates to be written to firebase-db
 
   const doctorId = ctx.params.doctorId;
+  const appt_date = ctx.params.appt_date;
   
   const dataSnapshot = change.after.val();
   const { patientId, patientName, profileName, dateOfBirth, gender, height, weight, ailment, apptTime, keys, notes} = dataSnapshot;
 
-
-  var newApptRef = admin.database().ref('appts').push();
-  var newApptRefKey = newApptRef.key;
-
+  // prepare update for which a new appt was added or modified (notes/rx/labs data added)
   var updateUserData = {};
-  updateUserData[`${patientId}/${profileName}/appts/${newApptRefKey}`] = { "apptTime": apptTime ,'ailment': ailment, 'keys': keys, "notes": notes || null };
+  updateUserData[`${patientId}/${profileName}/appts/${appt_date}`] = { "apptTime": apptTime ,'ailment': ailment, 'keys': keys, "notes": notes || null };
   updateUserData[`${patientId}/${profileName}/profile`] = {
     'patientName': patientName,
     'dateOfBirth': dateOfBirth,
@@ -34,55 +32,77 @@ exports.appointment = functions.database.ref('/appointments/{doctorId}/{date}/{t
     'height': height,
     'weight': weight,
     'keys': keys,
+    'patientId': patientId
   };
   updateUserData[`${patientId}/${profileName}/lastVisit`] = apptTime;
 
-  console.log('Update my_patients with data', updateUserData);
-
-  // check if patient with patientId exists
   const location = `doctors/${doctorId}/my_patients/`;
+  let patient_exists = false;
 
+  // check if the patient exists
   await admin.database().ref(`${location}/patient_list`).orderByChild('patientId').equalTo(patientId).once('value', async (snapshot) => {
     if (snapshot.exists()) {
-      
-      // // add patient data under its profile name
-      let profileExists = false;
-      await admin.database().ref(location).child(`${patientId}/${profileName}`).once('value', (snapshot) => {
-        if (snapshot.exists()) {
-          profileExists = true;
-        }
-      });
-
-      // new profile - set creation date on the profile
-      if (!profileExists) {
-        // set join date as today.
-        updateUserData[`${patientId}/${profileName}/created`] = dayjs().unix();
-      }
-      await updateAppts(location, profileName, updateUserData);
-
-    } else {
-      var newPatientRef = await admin.database().ref(`${location}/patient_list`).push();
-      var newPatientRefKey = newPatientRef.key;
-
-      // first time a patient and profile is being added so add
-      // created to profile
-    
-      updateUserData[`patient_list/${newPatientRefKey}`] = { patientId: patientId };
-      updateUserData[`${patientId}/${profileName}/created`] = apptTime;
-      await updateAppts(location, profileName, updateUserData);
+      patient_exists = true; 
     }
   })
+  
+  if (patient_exists) {
+    updateExistingPatient(location, patientId, profileName, updateUserData, apptTime);
+  } else {
+    addNewPatient(location, updateUserData, patientId, profileName, apptTime);
+  }
 
   return change.after.val();
 });
 
+
 /**
  * 
+ * @param {*} location 
+ * @param {*} patientId 
  * @param {*} profileName 
- * @param {*} apptData 
+ * @param {*} updateUserData 
  */
-async function updateAppts(location, profileName, apptData) {
-  await admin.database().ref(location).update(apptData, (error) => {
+async function updateExistingPatient(location, patientId, profileName, updateUserData, appt_time) {
+  let profileExists = false;
+  await admin.database().ref(location).child(`${patientId}/${profileName}`).once('value', (snapshot) => {
+    if (snapshot.exists()) {
+      profileExists = true;
+    }
+  });
+  // new profile - set creation date on the profile
+  if (!profileExists) {
+    // set join date as today.
+    updateUserData[`${patientId}/${profileName}/created`] = dayjs().unix();
+  }
+  updateAppt(location, updateUserData);
+}
+
+/**
+ * 
+ * @param {*} location 
+ * @param {*} updateUserData 
+ * @param {*} patientId 
+ * @param {*} profileName 
+ * @param {*} apptTime 
+ */
+async function addNewPatient(location, updateUserData, patientId, profileName, apptTime) {
+  var newPatientRef = await admin.database().ref(`${location}/patient_list`).push();
+  var newPatientRefKey = newPatientRef.key;
+  // first time a patient and profile is being added so add
+  // created to profile
+  updateUserData[`patient_list/${newPatientRefKey}`] = { patientId: patientId };
+  updateUserData[`${patientId}/${profileName}/created`] = apptTime;
+  updateAppt(location, updateUserData);
+}
+
+/**
+ * 
+ * @param {*} profile_name 
+ * @param {*} appt_data 
+ */
+async function updateAppt(location, appt_data) {
+  await admin.database().ref(location).update(appt_data, (error) => {
     if (error) {
       console.log('Unable to save new data for patient with patientId', patientId);
     }
